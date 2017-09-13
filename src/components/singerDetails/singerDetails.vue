6e239b01be2023558273b64324b42badca010782
houfujian <ApeLiftingIron@163.com>
2017-09-13 10:51:09 +0800

    singerdetails

---
+++ src/components/singerDetails/singerDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="singer-details">
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="stylus">
+  @import '~common/stylus/variable'
+
+  .singer-details
+    position: fixed
+    z-index: 100
+    width: 100%
+    height 100%
+    background color-theme
+</style>
