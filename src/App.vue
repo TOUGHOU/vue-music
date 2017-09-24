4eedacc653352ccf46c4201eed8bfd330e01ca0f
houfujian <ApeLiftingIron@163.com>
2017-09-24 23:39:14 +0800

    添加plaer组件

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,20 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <m-plaer></m-plaer>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/mhead/mhead'
   import MTab from 'components/tab/tab'
+  import MPlaer from 'components/player/player'
 
   export default {
     components: {
       MHeader,
-      MTab
+      MTab,
+      MPlaer
     }
   }
 </script>
