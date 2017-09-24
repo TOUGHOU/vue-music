ac75454841d1fc6fb9d0f3fa79fc3a60b28fd945
houfujian <ApeLiftingIron@163.com>
2017-09-24 12:10:16 +0800

    播放器vuex 配置

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,6 +5,12 @@
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div ref="playBtn" v-show="songs.length>0" class="play"  >
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="bgLayer"></div>
@@ -23,7 +29,9 @@
 <script type="text/ecmascript-6">
 import scroll from 'base/scroll/scroll'
 import songlist from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
 
+const transform = prefixStyle('transform')
 const TITLE_HEIGHT = 40
 
 export default {
@@ -73,16 +81,17 @@ export default {
         scale = 1 + percent
       }
       // 向上偏移
-      this.$refs.bgLayer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-      this.$refs.bgLayer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+      this.$refs.bgLayer.style[transform] = `translate3d(0, ${translateY}px, 0)`
       // 控制背景图片样式
       if (y < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.height = `${TITLE_HEIGHT}px`
         this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.height = 0
         this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.playBtn.style.display = ''
       }
 
       this.$refs.bgImage.style.transform = `scale(${scale})`
