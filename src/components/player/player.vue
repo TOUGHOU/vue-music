2cb60b683cda9e6a333d86adc85cf38e4eb2715e
houfujian <ApeLiftingIron@163.com>
2017-09-24 23:34:37 +0800

    创建播放器组件

---
+++ src/components/player/player.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="player" v-show="playlist.length>0">
+    <div class="normal-player" v-show="fullScreen">
+      播放器
+    </div>
+    <div class="mini-player" v-show="!fullScreen"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import {mapGetters} from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'fullScreen',
+      'playlist'
+    ])
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import "~common/stylus/variable"
+
+  .player
+    .normal-player
+      position: fixed
+      left: 0
+      right: 0
+      top: 0
+      bottom: 0
+      z-index: 150
+      background: $color-background
+      .background
+        position: absolute
+        left: 0
+        top: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        opacity: 0.6
+        filter: blur(20px)
+    .mini-player
+      display: flex
+      align-items: center
+      position: fixed
+      left: 0
+      bottom: 0
+      z-index: 180
+      width: 100%
+      height: 60px
+      background: $color-highlight-background
+</style>
